6fa035e3adcbe4b1ca84aef8fbd65df91f703553
Do-hyun Ko <dohyun682@gmail.com>
2024-01-29 21:27:36 +0900

    :bricks: Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import "./App.css";
-
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
 import styled, { CSSProperties } from "styled-components";
-
-import ReactLogo from "./assets/react.svg?react";
 
 const ReadTheDocs = styled.p<{
   $color?: CSSProperties["color"];
@@ -13,32 +7,10 @@ const ReadTheDocs = styled.p<{
 `;
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { t } = useTranslation("main");
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src="/src/assets/react.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <ReadTheDocs $color={"blue"}>
-        {t("title")}
-        <ReactLogo width={16} height={16} />
-      </ReadTheDocs>
+      <button className={"w-10 h-10 bg-slate-100 rounded-md"} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
