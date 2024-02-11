4af02aca34b97d6c276865fb4e1f4fe1c7edc59c
Do-hyun Ko <dohyun682@gmail.com>
2024-02-12 00:43:01 +0900

    :sparkles: Add issue page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import DocumentPage from "./pages/document/DocumentPage";
+import IssuePage from "./pages/issue/IssuePage";
 import PullRequestPage from "./pages/pullrequest/pullRequestPage";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/document" element={<DocumentPage />} />
           <Route path="/pullrequest" element={<PullRequestPage />} />
+          <Route path="/issue" element={<IssuePage />} />
         </Routes>
       </BrowserRouter>
       <div className="mb-4 " />
